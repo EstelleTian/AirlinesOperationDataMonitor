783508d42f3af5a1ca98bb5cd339af98f87ba90e
fengjianying <872808932@qq.com>
2017-11-23 09:58:24 +0800

    增加响应式小于900px后 出现滚动条 增加后台接口数据校验

--- js/dataMonitoring.js
+++ js/dataMonitoring.js
@@ -74,101 +74,99 @@ var getTotalDateCount =function () {
         }
     })
 };
-getTotalDateCount();
-
-
 /*echarts 绘制*/
-// 基于准备好的dom，初始化echarts实例
-var airportChart = echarts.init($("#airport_chart")[0]);
-var flightChart = echarts.init($("#flight_chart")[0]);
-var manageChart = echarts.init($("#manage_chart")[0]);
-var monitorChart = echarts.init($("#monitor_chart")[0]);
+// 初始化echarts曲线图
+var initCurveCharts = function () {
+    var airportChart = echarts.init($("#airport_chart")[0]);
+    var flightChart = echarts.init($("#flight_chart")[0]);
+    var manageChart = echarts.init($("#manage_chart")[0]);
+    var monitorChart = echarts.init($("#monitor_chart")[0]);
 
-// 指定图表的配置项和数据
-var option = {
-    backgroundColor: '#FFFFFF',
-    color: ['#3398DB'],
-    title: {
-        text: ''
-    },
-    grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '15%',
-        containLabel: true
-    },
-    tooltip: {},
-    legend: {
-        data: ['流控数量',"告警数量","**数量","***数量"],
-        bottom: "2"
-    },
-    xAxis: {
-        name: "时间",
-        data: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", '24', '25', '26', '27', '28', '29', '30'],
-        boundaryGap : false,
+// 获取图表的配置项和数据
+    var option = {
+        backgroundColor: '#FFFFFF',
+        color: ['#3398DB'],
+        title: {
+            text: ''
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '15%',
+            containLabel: true
+        },
+        tooltip: {},
+        legend: {
+            data: ['流控数量',"告警数量","**数量","***数量"],
+            bottom: "2"
+        },
+        xAxis: {
+            name: "时间",
+            data: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", '24', '25', '26', '27', '28', '29', '30'],
+            boundaryGap : false,
 
-    },
-    yAxis: {
-        name: "数量",
-    },
-    dataZoom: {
-        show: true,
-        start: 30,
-        zoomLock:false,
-        end: 60
-    },
-    series: [{
-        name: "流控数量",
-        type: 'line',
-        symbol:'none',
-        smooth:true,
-        border:0,
-        itemStyle: {normal: {areaStyle: {type: 'default'},color:"#9abcc3",lineStyle:{width:0}}},
-        data: [6, 39,20,85,7,39,20,85,39,20,101, 7,25,107,31,74,6,74,6,85,7,31,74,6,39,20,85,7,2]
-    }, {
-        name: "告警数量",
-        type: 'line',
-        symbol:'none',
-        smooth:true,
-        itemStyle: {normal: {areaStyle: {type: 'default'},color:"#CCCCFF",lineStyle:{width:0}}},
-        data: [31,74,6,39,20,85,7,31,74,45,9,70,35,7,61,24,96,39,20,85,7,31,74,6,39,20,85,7,26,45]
-    }, {
-        name: "**数量",
-        type: 'line',
-        symbol:'none',
-        smooth:true,
-        itemStyle: {normal: {areaStyle: {type: 'default'},color:"#9B59B6",lineStyle:{width:0}}},
-        data: [31,74,6,39,20,39,20,85,7,51,25,7,31,74,6,74,6,39,20,85,7,26,85,7,31,74,6,39,45,5]
-    }, {
-        name: "***数量",
-        type: 'line',
-        symbol:'none',
-        smooth:true,
-        itemStyle: {normal: {areaStyle: {type: 'default'},color:"#1ABB9C",lineStyle:{width:0}}},
-        data: [31,74,6,39,20,74,6,39,74,6,65,15,85,7,50,2,85,7,26,85,7,31,31,74,6,39,20,85,7]
-    }]
-};
-// 使用刚指定的配置项和数据显示图表。
-airportChart.setOption(option);
-flightChart.setOption(option);
-manageChart.setOption(option);
-monitorChart.setOption(option);
+        },
+        yAxis: {
+            name: "数量",
+        },
+        dataZoom: {
+            show: true,
+            start: 30,
+            zoomLock:false,
+            end: 60
+        },
+        series: [{
+            name: "流控数量",
+            type: 'line',
+            symbol:'none',
+            smooth:true,
+            border:0,
+            itemStyle: {normal: {areaStyle: {type: 'default'},color:"#9abcc3",lineStyle:{width:0}}},
+            data: [6, 39,20,85,7,39,20,85,39,20,101, 7,25,107,31,74,6,74,6,85,7,31,74,6,39,20,85,7,2]
+        }, {
+            name: "告警数量",
+            type: 'line',
+            symbol:'none',
+            smooth:true,
+            itemStyle: {normal: {areaStyle: {type: 'default'},color:"#CCCCFF",lineStyle:{width:0}}},
+            data: [31,74,6,39,20,85,7,31,74,45,9,70,35,7,61,24,96,39,20,85,7,31,74,6,39,20,85,7,26,45]
+        }, {
+            name: "**数量",
+            type: 'line',
+            symbol:'none',
+            smooth:true,
+            itemStyle: {normal: {areaStyle: {type: 'default'},color:"#9B59B6",lineStyle:{width:0}}},
+            data: [31,74,6,39,20,39,20,85,7,51,25,7,31,74,6,74,6,39,20,85,7,26,85,7,31,74,6,39,45,5]
+        }, {
+            name: "***数量",
+            type: 'line',
+            symbol:'none',
+            smooth:true,
+            itemStyle: {normal: {areaStyle: {type: 'default'},color:"#1ABB9C",lineStyle:{width:0}}},
+            data: [31,74,6,39,20,74,6,39,74,6,65,15,85,7,50,2,85,7,26,85,7,31,31,74,6,39,20,85,7]
+        }]
+    };
+// 曲线图参数设置。
+    airportChart.setOption(option);
+    flightChart.setOption(option);
+    manageChart.setOption(option);
+    monitorChart.setOption(option);
+    //适应屏幕宽高尺寸
+    window.onresize = function () {
+        //重置容器高宽
+        resizeWorldMapContainer();
+        airportChart.resize();
+        flightChart.resize();
+        manageChart.resize();
+        monitorChart.resize();
+        resetML($('.airports_container'),$('.as'));
+        resetML($('.flights_container'),$('.fs'));
+    };
+}
 //用于使chart自适应高度和宽度,通过窗体高宽计算容器高宽
-
 var resizeWorldMapContainer = function () {
-    $("#airport_chart").width = window.innerWidth+'px';
-    $("#airport_chart").height = window.innerHeight+'px';
-};
-//适应屏幕宽高尺寸
-window.onresize = function () {
-    //重置容器高宽
-    resizeWorldMapContainer();
-    airportChart.resize();
-    flightChart.resize();
-    manageChart.resize();
-    monitorChart.resize();
-    // resetML($('.airports_container'),$('.as'));
-    // resetML($('.flights_container'),$('.fs'));
+    $("#airport_chart").width (window.innerWidth);
+    $("#airport_chart").height (window.innerHeight);
 };
 /*机场运行信息*/
 var setAirportsInformation = function (fatherDom) {
@@ -197,7 +195,6 @@ var setAirportsInformation = function (fatherDom) {
         }
     })
 }
-setAirportsInformation($(".airports_container").find(".row"));
 /*航空公司运行信息*/
 var setFlightsInformation = function (fatherDom) {
     $.ajax({
@@ -225,7 +222,6 @@ var setFlightsInformation = function (fatherDom) {
         }
     })
 }
-setFlightsInformation($(".flights_container"))
 /*机场&&航空公司布局适配*/
 function resetML(father,son){
     var aW = father.width();
@@ -234,32 +230,38 @@ function resetML(father,son){
     var mL = (aW - bW*bN)/(bN+1);
     son.css('margin-left', mL+'px')
 }
-// resetML();
-
-
-/*导航栏点击事件*/
-$(".monitoring").click(function () {
-    $(this).addClass("item_selected")
-    $(".search").removeClass("item_selected")
-});
-$(".search").click(function () {
-    $(this).addClass("item_selected")
-    $(".monitoring").removeClass("item_selected")
-})
-$(".monitoring_sm").click(function () {
-    $(this).addClass("item_selected")
-    $(".search_sm").removeClass("item_selected")
-});
-$(".search_sm").click(function () {
-    $(this).addClass("item_selected")
-    $(".monitoring_sm").removeClass("item_selected")
-})
-$("#switch_nav").click(function () {
-    $(".navigation").toggle();
-    $(".navigation_sm").toggle();
-    airportChart.resize();
-    flightChart.resize();
-    manageChart.resize();
-    monitorChart.resize();
+/*导航栏初始化*/
+var initNavigator = function () {
+    $(".monitoring").click(function () {
+        $(this).addClass("item_selected")
+        $(".search").removeClass("item_selected")
+    });
+    $(".search").click(function () {
+        $(this).addClass("item_selected")
+        $(".monitoring").removeClass("item_selected")
+    })
+    $(".monitoring_sm").click(function () {
+        $(this).addClass("item_selected")
+        $(".search_sm").removeClass("item_selected")
+    });
+    $(".search_sm").click(function () {
+        $(this).addClass("item_selected")
+        $(".monitoring_sm").removeClass("item_selected")
+    })
+    $("#switch_nav").click(function () {
+        $(".navigation").toggle();
+        $(".navigation_sm").toggle();
+        airportChart.resize();
+        flightChart.resize();
+        manageChart.resize();
+        monitorChart.resize();
+    })
+}
+$(document).ready(function () {
+    initNavigator();//初始化导航栏
+    getTotalDateCount();//获取航班监控页面数据
+    initCurveCharts();//初始化曲线图
+    setAirportsInformation($("#airports_container"));//机场运行监控
+    setFlightsInformation($("#flights_container"));//航空公司运行监控
 })
 
