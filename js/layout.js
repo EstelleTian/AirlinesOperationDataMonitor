9db38763c7557a0ae80484e9dacaf6d3b7d26b59
krorainaHub <zxy920411@163.com>
2017-11-29 14:23:08 +0800

    导航栏事件绑定

---
+++ js/layout.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    //导航栏
+
+   var nav = $('#nav');
+    // 当日运行数据监控
+
+    // 历史运行数据量统计
+   $('.nav-history-data-statistics').on('click',function () {
+
+      $('li',nav).removeClass('active');
+      $(this).addClass('active');
+      $('#body .row').removeClass('active');
+      $('#history-data-statistics').addClass('active');
+
+
+   });
+    // 运行数据查询
+   $('.nav-operating-data-query').on('click',function () {
+       $('li',nav).removeClass('active');
+       $(this).addClass('active');
+       $('#body .row').removeClass('active');
+       $('#operating-data-query').addClass('active');
+   });
+
+
+    // 登出
+
+});
